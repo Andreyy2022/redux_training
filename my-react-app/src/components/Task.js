3b8f26ae7e2529b26243ac2d267383056cece44f
Andreyy2022 <104273434+Andreyy2022@users.noreply.github.com>
2024-03-08 14:06:50 +0300

    8

---
+++ my-react-app/src/components/Task.js
@@ -0,0 +1,25 @@
+import React from "react";
+import {useDispatch} from 'react-redux';
+import {Form, ListGroup, removeTask, toggleTask} from '../store/action';
+
+const Task = ({task}) => {
+    const {id, title, completed} = task;
+    const dispatch = useDispatch();
+
+    return (
+        <ListGroup.Item className={completed && 'task-completed'}>
+            <Form.Check
+                id={id}
+                type='checkbox'
+                label={title}
+                checked={completed}
+                onChange={() => dispatch(toggleTask(id))}
+            />
+            <div className="list-group-actions">
+                <span onClick={() => dispatch(removeTask(id))}>Удалить</span>
+            </div>
+        </ListGroup.Item>
+    )
+}
+
+export default Task;
--- my-react-app/src/store/store.js
+++ my-react-app/src/store/store.js
@@ -4,7 +4,7 @@ import reducer from from "./reducer";
 const store = createStore(
     reducer,
     window.__REDUX_DEVTOOLS_EXTENSION__&&
-window.__REDUX_DEVTOOLS_EXTENTION__()
+    window.__REDUX_DEVTOOLS_EXTENTION__()
 );
 
 export default store;
