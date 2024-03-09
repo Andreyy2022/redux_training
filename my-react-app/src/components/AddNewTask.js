6084be7c6d6858488eba5064b5fced4d229d904e
Andreyy2022 <104273434+Andreyy2022@users.noreply.github.com>
2024-03-09 13:56:55 +0300

    10

---
+++ my-react-app/src/components/AddNewTask.js
@@ -0,0 +1,30 @@
+import React, {useState} from "react";
+import { Button, FormControl, InputGroup } from "react-bootstrap";
+import * as action from '../store/action';
+import { UseDispatch, useDispatch } from "react-redux";
+
+const AddNewTask = () => {
+    const [taskTitle, setTaskTitle] = useState('');
+    const dispatch = useDispatch();
+
+    const handleTaskTitleChange = (e) => {
+        setTaskTitle(e.target.value);
+    }
+
+    const handleTaskSubmit = () => {
+        dispatch(actions.addTask({
+            title: taskTitle
+        }));
+    }
+
+    return (
+        <InputGroup className="mb-3">
+            <FormControl placeholder="Добавить новый текст" value={taskTitle} onChange={e => handleTaskTitleChange(e)} />
+            <InputGroup.Append>
+                <Button variant="outline-secondsry" onClick={handleTaskSubmit}>Сохранить</Button>
+            </InputGroup.Append>
+        </InputGroup>
+    )
+}
+
+export default AddNewTask;
